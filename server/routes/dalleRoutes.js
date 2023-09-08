76e6849178de6c056f010b3ae16680baf51efc64
Aryan Bansod <aryanem09@gmail.com>
2023-09-08 13:04:39 +0530

    err

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,7 +1,14 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI from "openai";
+
+const openAI = new OpenAIApi();
+const openai = new OpenAIApi({
+  apiKey: process.env.OPEN_API_KEY,
+});
 
 dotenv.config();
 
 const router = express.Router();
+
+export default router;
